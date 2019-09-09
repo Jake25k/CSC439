f12466a06c56db482cb337c6f04a0e117feb0c96
Jacob Moore <moorej37@mymail.nku.edu>
2019-09-09 19:29:21 -0400

    test php db connection

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 
 <?php
-  include("mysqli_oop_connect");
+
   include_once("Index.html");
+
+  require("mysqli_oop_connect.php");
+  $q = "INSTER INTO Persons values (NULL, 'Jake')";
+  $r = @$mysqli->query($q);
 ?>
