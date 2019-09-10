1a3e2d1bbbc171a32db47e04204db8142634fbac
Jacob Moore <moorej37@mymail.nku.edu>
2019-09-09 20:48:08 -0400

    Update users.php

--- users.php
+++ users.php
@@ -1,3 +1,20 @@
 <?php
-  echo "Hello";
+
+$conn = pg_pconnect("dbname=db8u3gdkjq4l6i");
+if (!$conn) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+$result = pg_query($conn, "SELECT firstName FROM users");
+if (!$result) {
+  echo "An error occurred.\n";
+  exit;
+}
+
+while ($row = pg_fetch_row($result)) {
+  echo "First name: $row[0]";
+  echo "<br />\n";
+}
+
 ?>
