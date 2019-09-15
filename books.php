b378d21701491081ce49e5d31805da3d000eb828
ladundead1988 <craigj4@mymail.nku.edu>
2019-09-15 16:32:15 -0400

    Update books.php

--- books.php
+++ books.php
@@ -27,7 +27,35 @@
 
 <header>
 <h1>All Books</h1>
-<a href="index.php">return to main page</a>
+    <div class="container-fluid p-0">
+      <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand" href="#">
+          <i class="fas fa-book-reader fa-2x mx-3"></i>Books</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <i class="fas fa-align-right text-light"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mr-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">HOME
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href='books.php'>BOOK INVENTORY</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href='users.php'>USERS</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">ABOUT</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
 </header>
 
 <main>
