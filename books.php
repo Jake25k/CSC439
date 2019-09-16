ec808ce31a8057c5ecf0b7bb6b2fa15e5820ba71
mcguirej3 <38018349+mcguirej3@users.noreply.github.com>
2019-09-15 23:48:25 -0400

    Update books.php

--- books.php
+++ books.php
@@ -78,11 +78,6 @@ if(!$query){
   while($results = pg_fetch_array($query, NULL, PGSQL_ASSOC)){
     echo "<tr>";
     echo "<td>" . $results['title'] . "</td>";
-	?>
-	<a class="nav-link" href="description.php">Description
-	<span class="sr-only">(current)</span>
-	</a>
-	<?php
     echo "<td>" . $results['author'] . "</td>";
     echo "<td>" . $results['isbn'] . "</td>";
     echo "</tr>";
@@ -91,6 +86,9 @@ if(!$query){
 }
 
 ?>
+	<a class="nav-link" href="description.php">Description
+	<span class="sr-only">(current)</span>
+	</a>
 </main>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
