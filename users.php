6256928687a06c429e69ae448827d76c1d2141c7
spraguez1 <32646325+spraguez1@users.noreply.github.com>
2019-09-22 11:59:09 -0400

    Update users.php

--- users.php
+++ users.php
@@ -60,7 +60,8 @@
 		<ul style="list-style-type:none;">
 			<li><b>Username:</b></li><li><input type="text" name="uname" /></li>
 			<li><b>Password:</b></li><li><input type="password" name="pass" /></li>
-			<li><b>Confirm Password:</b></li><li><input type="password" name="confirmPass" /></li>				
+			<li><b>Confirm Password:</b></li><li><input type="password" name="confirmPass" /></li>
+			<li>Already have account<a href="users.php">click here</a></li>
 			<li><input type="submit" /></li>
 			
 		</ul>		
@@ -83,12 +84,12 @@ if (!$result) {
   echo "An error occurred.\n";
   exit;
 }
-
+/*
 while ($row = pg_fetch_row($result)) {
   echo "First name: $row[0]";
   echo "<br />\n";
 }
-
+*/
 ?>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
