c61b03eec8417318d303a0729fef2377f6c25c5e
mcguirej3 <38018349+mcguirej3@users.noreply.github.com>
2019-09-22 19:52:14 -0400

    Update descriptions.php

--- descriptions.php
+++ descriptions.php
@@ -54,20 +54,7 @@
         </div>
       </nav>
     </div>
-    <div class="container text-center">
-      <div class="row">
-        <div class="col-md-7 col-sm-12  text-white">
-          <h1>ABOUT US!</h1>
-          <h6>If anyone knows books, it's us!</h6>
-          <p>
-            We love them books! Buy from us!
-          </p>
-        </div>
-        <div class="col-md-5 col-sm-12  h-25">
-          <img src="./assets/header-img.png" alt="Book" />
-        </div>
-      </div>
-    </div>
+
 </header>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
