c542d2d723baa9c0b301ca424c997530984a899b
spraguez1 <32646325+spraguez1@users.noreply.github.com>
2019-09-22 18:00:16 -0400

    Made login button

--- login.php
+++ login.php
@@ -56,44 +56,19 @@
     </div>
 </header>
 
-<!-- User login and new user
-	<div class="newUser">
-		<h2>Create New User</h2>
-		<ul style="list-style-type:none;">
-			<li><b>Username:</b></li><li><input type="text" name="uname" /></li>
-			<li><b>Password:</b></li><li><input type="password" name="pass" /></li>
-			<li><b>Confirm Password:</b></li><li><input type="password" name="confirmPass" /></li>
-			<li>Already have account<a href="users.php"> click here</a></li>
-			<li><input type="submit" /></li>
-			
-		</ul>		
-	</div>
--->
-
-<?php
-// Connect to the database
-$conn = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
-if (!$conn) {
-  echo "An error occurred.\n";
-  exit;
-}
-
-// Insert user into database
-// $q = "INSERT INTO user VALUES ('$_POST[uname]','$_POST[pass]')";
-// $r = pg_query($q);
-
-$result = pg_query($conn, "SELECT firstName FROM users");
-if (!$result) {
-  echo "An error occurred.\n";
-  exit;
-}
-/*
-while ($row = pg_fetch_row($result)) {
-  echo "First name: $row[0]";
-  echo "<br />\n";
-}
-*/
-?>
+<div class="login-page">
+<div class="form">
+	<form class="register">
+		<ul style="list-style-type: none";>
+			<li><label for="uname"><b>Username</b></label></li>
+			<input type="text">
+			<li><label for="pass"><b>Password</b></label></li>
+			<input type="password">
+			<li><button type="submit">Login</button></li>
+		</ul>
+	</form>
+</div>
+</div>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
