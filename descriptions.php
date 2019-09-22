61d61c5e0c10be0e30c172b8271f0a3fbaac6dee
mcguirej3 <38018349+mcguirej3@users.noreply.github.com>
2019-09-22 19:02:15 -0400

    Update descriptions.php

--- descriptions.php
+++ descriptions.php
@@ -1,35 +1,24 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Books</title>
+
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossorigin="anonymous" />
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
     crossorigin="anonymous">
   <link rel="stylesheet" href="styles/Style.css" />
-  <style>
-    header{
-      padding-left: 5px;
-      background: var(--bggradient);
-      width: 100%;
-    }
-    table{
-      margin: 10px;
-      padding: 5px;
-    }
-    td{
-      padding-right: 15px;
-    }
-  </style>
+  <link rel="stylesheet" href="styles/mobile-style.css">
 </head>
 
-
 <body>
-
-<header>
+  <header>
     <div class="container-fluid p-0">
       <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="Index.html">
           <i class="fas fa-book-reader fa-2x mx-3"></i>Books</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
           aria-label="Toggle navigation">
@@ -48,12 +37,12 @@
               <a class="nav-link" href="Index.html">HOME</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="books.php">BOOK INVENTORY
-				<span class="sr-only">(current)</span>
-			  </a>
+              <a class="nav-link" href="books.php">BOOK INVENTORY</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="users.php">USERS</a>
+              <a class="nav-link" href="users.php">USERS
+				<span class="sr-only">(current)</span>
+			  </a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="about.html">ABOUT</a>
@@ -66,6 +55,7 @@
       </nav>
     </div>
 </header>
+
 <?php
 
 db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
