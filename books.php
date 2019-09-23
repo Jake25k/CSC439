e40e0d855aebcd3af5571f36f51fad9a2dd1b3a2
ladundead1988 <craigj4@mymail.nku.edu>
2019-09-23 11:41:10 -0400

    Update books.php

--- books.php
+++ books.php
@@ -33,15 +33,15 @@
 	}
 	table.table-bordered{
 		border:2px solid black;
-		width: 33% !important;
+		width: 100% !important;
 		margin-top:20px;
 	}
 	table.table-bordered > thead > tr > th{
-		width: 33% !important;
+		width: 100%% !important;
 		border:2px solid black;
 	}
 	table.table-bordered > tbody > tr > td{
-		width: 33% !important;
+		width: 100% !important;
 		border:2px solid black;
 	}
 
