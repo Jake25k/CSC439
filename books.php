1fab715b71b700dd98244bb9fa7cbcc6b6b1164e
ladundead1988 <craigj4@mymail.nku.edu>
2019-09-23 10:10:11 -0400

    Update books.php

--- books.php
+++ books.php
@@ -13,14 +13,9 @@
       background: var(--bggradient);
       width: 100%;
     }
-/* 	h1 {
-		text-align: center;
-	} */
     table{
-      /*margin: auto;*/
 	  margin: 10px;
       padding: 5px;
-	  /* width: 50% !important; */
 	  
     }
     td{
