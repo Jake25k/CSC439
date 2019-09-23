4e9a8dbb2bc08832510ba63632afba1a06844cc1
Chris Wells <wellsj12@mymail.nku.edu>
2019-09-23 16:46:59 -0400

    Update search.php

--- search.php
+++ search.php
@@ -72,7 +72,7 @@ if (!empty($_POST['term'])) {
     $term = $_POST['term'];  
     $type = $_POST['SearchType'];
     
-    echo '%$type%';
+    echo '$type';
     
     $query = pg_query($conn, "SELECT * FROM books WHERE UPPER('%$type%') LIKE UPPER('%$term%')");
     
