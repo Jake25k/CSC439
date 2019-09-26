6ac7acc5b06ebc02bde023500a3f5886674d269e
Jacob Moore <moorej37@mymail.nku.edu>
2019-09-26 01:28:48 -0400

    Added test directory for unit testing methods for sprint3

---
+++ tests/unit/sprint3tests.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests\unit;
+
+class UnitTests extends \PHPUnit\Framework\TestCase
+{
+  public function testTrueIsTrue()
+  {
+    $foo = true;
+    $this->assertTrue($foo);
+  }
+}
+
+?>
