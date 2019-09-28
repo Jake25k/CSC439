4e3366df0ccffdb4bd1a34e1acc31807e53e4c19
ladundead1988 <craigj4@mymail.nku.edu>
2019-09-28 19:09:55 -0400

    Update books.php

--- books.php
+++ books.php
@@ -98,7 +98,7 @@
 <h1>All Books</h1>
 <main>
 <?php
-$display = 10
+
 $db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
 $query = pg_query($db_con, "SELECT * from books");
 
