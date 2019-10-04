beaf42999451aa1e90fcba0cdb2b44bf9e9b686c
Jacob Moore <moorej37@mymail.nku.edu>
2019-10-04 16:36:42 -0400

    made view page look nice

--- view.php
+++ view.php
@@ -12,75 +12,95 @@
 
 
 <body>
-<header>
-    <div class="container-fluid p-0">
-      <nav class="navbar navbar-expand-lg">
-        <a class="navbar-brand" href="Index.html">
-          <i class="fas fa-book-reader fa-2x mx-3"></i>Best Books</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <i class="fas fa-align-right text-light"></i>
-        </button>
-        <div class="search">
-          <form action="search.php" method="post">
-              <input type="text" name="term" />
-              <input type="submit" value="Search" />
+  <header>
+      <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg">
+          <a class="navbar-brand" href="Index.html">
+            <i class="fas fa-book-reader fa-2x mx-3"></i>Best Books</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-align-right text-light"></i>
+          </button>
+          <div class="search">
+            <form action="search.php" method="post">
+                <input type="text" name="term" />
+                <input type="submit" value="Search" />
 
-              <select name="type">
-                <option value="0">Title</option>
-                <option value="1">Author</option>
-                <option value="2">ISBN</option>
-              </select>
-          </form>
-        </div>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="mr-auto"></div>
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="Index.html">HOME</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="books.php">BOOK INVENTORY
-				<span class="sr-only">(current)</span>
-			  </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="about.html">ABOUT</a>
-            </li>
-			<li class="nav-item">
-              <a class="nav-link" href="login.php">LOGIN</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+                <select name="type">
+                  <option value="0">Title</option>
+                  <option value="1">Author</option>
+                  <option value="2">ISBN</option>
+                </select>
+            </form>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="mr-auto"></div>
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="Index.html">HOME</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="books.php">BOOK INVENTORY
+  				<span class="sr-only">(current)</span>
+  			  </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">ABOUT</a>
+              </li>
+  			<li class="nav-item">
+                <a class="nav-link" href="login.php">LOGIN</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+  </header>
+  <?php
+
+  $id = $_GET['id']; //get the book id from the url
+
+  $db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
+  $result = pg_query($db_con, "SELECT * FROM books WHERE book_id=" . $id . ";");
+  if (!$result) {
+    echo "An error occurred.\n";
+    exit;
+  }
+  else{
+    $row = pg_fetch_row($result);
+  }
+  ?>
+
+<div style="display: flex; justify-content: center;">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <?php echo "<br><div style='text-align: center;'><h1> Title: $row[0]</h1></div>"; ?>
     </div>
-</header>
-<main>
-<?php
+    <div class="panel-body">
+      <div class="row">
+        <div class="col-md-4">
+          <?php echo "<img src='assets/covers/$row[4]' style='height: 360px;'/>";
+                echo "<br><h5>ISBN: $row[2]</h5>";
+                echo "<button class='btn btn-primary'>Add to Cart</button><br><br>";
+                echo "<button class='btn btn-success'>Add to Wish List</button><br><br>";
+                echo "<button class='btn btn-danger'>Add to Watch List</button><br><br>";
+                ?>
+        </div>
+        <div class = "col-md-6">
+          <?php echo "<h3> Author: <a href='#'>$row[1]</a></h3><br>";
+                echo "<h2>Price: $row[6]</h2><br>";
+                echo "<h5> Description:<br>$row[5] </h5>";
+                ?>
+                <br>
+                <br>
+                <br>
+                <br>
+                <h4>Reccomended books: </h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-$id = $_GET['id']; //get the book id from the url
-
-$db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
-$result = pg_query($db_con, "SELECT * FROM books WHERE book_id=" . $id . ";");
-if (!$result) {
-  echo "An error occurred.\n";
-  exit;
-}
-else{
-  $row = pg_fetch_row($result);
-  echo "<div style='text-align:center;'>";
-  echo "<br />";
-  echo "<img src='assets/covers/$row[4]' style='height: 160px;'/>";
-  echo "<h1> Title: $row[0]</h1>";
-  echo "<h3> Author: $row[1]</h3>";
-  echo "<h4> ISBN: $row[2]</h4>";
-  echo "<br />\n";
-  echo "<button class='btn btn-primary'>Add to Cart</button>";
-  echo "</div>";
-}
-?>
-
-</main>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
     crossorigin="anonymous"></script>
