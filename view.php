828185968eb805bde6ce0052e6fbc3668a07cf80
z-sprague <32646325+z-sprague@users.noreply.github.com>
2019-10-19 16:37:56 -0400

    Update view.php

--- view.php
+++ view.php
@@ -1,5 +1,5 @@
 
-  </header>
+
   <?php
 	include('includes/header.php');
   $id = $_GET['id']; //get the book id from the url
@@ -14,6 +14,7 @@
     $row = pg_fetch_row($result);
   }
   ?>
+  </header>
 <div style="display: flex; justify-content: center;">
   <div class="panel panel-default">
     <div class="panel-heading">
@@ -45,11 +46,4 @@
   </div>
 </div>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-    crossorigin="anonymous"></script>
-  <script src="js/main.js"></script>
-</body>
-</html>
+<?php include('includes/footer.html'); ?>
