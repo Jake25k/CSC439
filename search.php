44ac31bbc834f5b8aaf0fa3b2ffed934ab28e0e7
Chris Wells <wellsj12@mymail.nku.edu>
2019-10-21 18:29:08 -0400

    Update search.php

--- search.php
+++ search.php
@@ -142,6 +142,7 @@ else {
 }
 ?>
 </main>
+<?php include('includes/footer.html'); ?>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
     crossorigin="anonymous"></script>
