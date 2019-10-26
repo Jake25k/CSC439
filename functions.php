62b3d678de51b2522cd2051240df25fe5fe89527
z-sprague <32646325+z-sprague@users.noreply.github.com>
2019-10-26 13:48:48 -0400

    Update function.php to retrieve number of users and number of books from the database

--- functions.php
+++ functions.php
@@ -1,7 +1,10 @@
 <?php
 
-/* Database connection */
+/* Global variable for database connection */
+$db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i
+			user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
 
+/* Functions for login.php */
 
 /* Gets the users username and password and removes any unnecessary characters */
 	function userInfo($uname, $pass){
@@ -27,19 +30,22 @@
 		}
 	}
 
+	/* Functions for index.php */
 	function getNumUsers(){
-		$db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i
-					user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
+		global $db_con;
 		$query = pg_query($db_con, "SELECT COUNT(*) from users");
 		$result = pg_fetch_row($query);
 		return $result[0];
 	}
 
 	function getNumBooks(){
-		$db_con = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i
-					user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
+		global $db_con;
 		$query = pg_query($db_con, "SELECT COUNT(*) from books");
 		$result = pg_fetch_row($query);
 		return $result[0];
 	}
+
+	/* Functions for register.php */
+
+
 ?>
