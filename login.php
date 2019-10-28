2ee0181a8ddb4902e9cea9a78d58fc87a6bde1f4
ZackS <zack.sprague1022@gmail.com>
2019-10-28 18:43:26 -0400

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     session_start();
     $pageTitle = 'Login';
 	include('includes/header.php');
-    include('loginFunctions.php');
+    include('functions.php');
 
 	/* Connect to database */
 	$dbc = pg_connect("host=ec2-54-235-100-99.compute-1.amazonaws.com port=5432 dbname=db8u3gdkjq4l6i user=oihnrigiktbsug password=03f8fa546db912cfc133c1faa898ef14cd26324691f4ba13ee09d89db73c9e8f");
