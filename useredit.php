f6ebdf9e1de0d015cd89351632401dc520442709
ladundead1988 <craigj4@mymail.nku.edu>
2019-11-11 18:55:21 -0500

    Update useredit.php

--- useredit.php
+++ useredit.php
@@ -24,58 +24,9 @@
       updateUser($uname, $fname, $lname);
     }
 ?>
-<head>
-  <meta charset="UTF-8" />
-  <title>User page</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-    crossorigin="anonymous" />
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
-    crossorigin="anonymous">
-  <link rel="stylesheet" href="styles/Style.css" />
-
-  <?php include("includes/userpage_functions.php"); ?>
-</head>
-
-<body>
-  <header>
-      <div class="container-fluid p-0">
-        <nav class="navbar navbar-expand-lg">
-          <a class="navbar-brand" href="index.php">
-            <i class="fas fa-book-reader fa-2x mx-3"></i>Best Books</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <i class="fas fa-align-right text-light"></i>
-          </button>
-        <div class="search">
-          <form action="search.php" method="post">
-              <input type="text" name="term" placeholder="Search..."/>
-              <input type="submit" value="Search" />
-          </form>
-        </div>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="mr-auto"></div>
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="books.php">BOOK INVENTORY
-  				<span class="sr-only">(current)</span>
-  			  </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.html">ABOUT</a>
-              </li>
-              <?php
-                if (isset($_SESSION['user'])) {
-                  echo '<li class="nav-item"><a class="nav-link" href="logout.php">LOGOUT</a></li>';
-                  echo '<li class="nav-item"><a class="nav-link" href="userpage.php">' . $_SESSION['user'] . '<br>Cart: '. count($_SESSION['user_cart']) .' Books</a></li>';
-                }
-                else {
-                  echo '<li class="nav-item"><a class="nav-link" href="login.php">LOGIN</a></li>';
-                }
-              ?>
-            </ul>
-          </div>
-        </nav>
-      </div>
+<?php
+	include("includes/header.php");
+?>
   </header>
 
 <?php
